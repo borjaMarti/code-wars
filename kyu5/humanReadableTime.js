07300e1b0c584d1a982484af279076a49e9c8f33
Borja Martí <borjamarti@outlook.com>
2023-01-30 14:14:58 +0100

    New kyu5 involving time format

---
+++ kyu5/humanReadableTime.js
@@ -0,0 +1,21 @@
+// 5 kyu
+// Human Readable Time
+// JavaScript:
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+//     HH = hours, padded to 2 digits, range: 00 - 99
+//     MM = minutes, padded to 2 digits, range: 00 - 59
+//     SS = seconds, padded to 2 digits, range: 00 - 59
+
+// The maximum time never exceeds 359999 (99:59:59)
+
+function humanReadable(seconds) {
+    let minutes = Math.floor(seconds / 60) % 60;
+    let hours = Math.floor(seconds / 3600);
+    seconds = seconds % 60;
+    
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+// Happy with my solution.
