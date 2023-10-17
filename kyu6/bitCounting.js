ab304fe71cc6649209cafcfc5f4659320dfa425d
Borja Martí <borjamarti@outlook.com>
2023-10-18 00:16:59 +0200

    new kyu6 converting integer to binary

---
+++ kyu6/bitCounting.js
@@ -0,0 +1,18 @@
+// 6 kyu
+// Bit Counting
+// JavaScript:
+
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.You can guarantee that input is non - negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case.
+
+const countBits = (n) => {
+  return n
+    .toString(2)
+    .split("")
+    .reduce((a, e) => a + +e, 0);
+};
+
+// I thought this was clever too:
+
+// const countBits = (n) => n.toString(2).split("0").join("").length;
