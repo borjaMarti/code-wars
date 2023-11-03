b53ff7559884561b4f4fbfee626934260213ccac
Borja Martí <borjamarti@outlook.com>
2023-11-03 20:47:44 +0100

    kyu6 iteration, math methods, and math algorithm

---
+++ kyu6/isNumberPrime.js
@@ -0,0 +1,27 @@
+// 6 kyu
+// Is a number prime ?
+// JavaScript:
+
+// Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+// Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+// Requirements
+
+//     You can assume you will be given an integer input.
+//     You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+//     NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+
+// is_prime(1)  /* false */
+// is_prime(2)  /* true  */
+// is_prime(-1) /* false */
+
+function isPrime(num) {
+  if (num === 2) return true;
+  if (num % 2 === 0 || num < 0 || num === 1) return false;
+
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
