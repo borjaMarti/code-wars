b863d791e3f764bf8ac1095660664d56c12e2b2a
Borja Martí <borjamarti@outlook.com>
2023-11-11 23:06:52 +0100

    new kyu 7 with sliding window algorithm for sum of lowest pair of integers in array

---
+++ kyu7/sumOfTwoLowest.js
@@ -0,0 +1,23 @@
+// 7 kyu
+// Sum of two lowest positive integers
+// JavaScript
+
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// function sumTwoSmallestNumbers(numbers) {
+//   let min1 = Infinity;
+//   let min2 = Infinity;
+
+//   for (let n of numbers) {
+//     const max = Math.max(min1, min2, n);
+//     if (max !== n) {
+//       max === min1 ? (min1 = n) : (min2 = n);
+//     }
+//   }
+
+//   return min1 + min2;
+// }
