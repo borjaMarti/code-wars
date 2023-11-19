d9b5ef15bc0f99f693e7a6f1eb52b4b5974c0d87
Borja Martí <borjamarti@outlook.com>
2023-11-19 19:24:22 +0100

    new kyu6 involving aplhabet and string manipulation

---
+++ kyu6/romanDecoder.js
@@ -0,0 +1,45 @@
+// 6 kyu
+// Roman Numerals Decoder
+// JavaScript:
+
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// "MM"      -> 2000
+// "MDCLXVI" -> 1666
+// "M"       -> 1000
+// "CD"      ->  400
+// "XC"      ->   90
+// "XL"      ->   40
+// "I"       ->    1
+
+function solution(roman) {
+  const numerals = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  let integer = 0;
+
+  for (let i = 0; i < numerals.length; i++) {
+    if (roman.slice(0, numerals[i][1].length) === numerals[i][1]) {
+      integer += numerals[i][0];
+      roman = roman.slice(numerals[i][1].length);
+      --i;
+    }
+  }
+
+  return integer;
+}
