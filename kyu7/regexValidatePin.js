8631b516f02c30f46f9b505dd4191a0151eed344
Borja Martí <borjamarti@outlook.com>
2024-01-15 22:32:41 +0100

    new kyu7 involving regex

---
+++ kyu7/regexValidatePin.js
@@ -0,0 +1,21 @@
+// 7 kyu
+// Regex validate PIN code
+// JavaScript:
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN(pin) {
+  return /^\d{4}(\d{2})?$/.test(pin);
+}
+
+// Alternative:
+
+// function validatePIN(pin) {
+//   return /^\d{4}(\d{2})?$/.test(pin);
+// }
