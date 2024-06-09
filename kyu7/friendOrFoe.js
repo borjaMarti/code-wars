ef5822a24690dafe5ce4d9bf6f15c15bdf4cd142
Borja Martí <borjamarti@outlook.com>
2024-06-09 23:57:44 +0200

    new kyu7 witth array manipulation

---
+++ kyu7/friendOrFoe.js
@@ -0,0 +1,15 @@
+// 7 kyu
+// Friend or Foe ?
+// JavaScript:
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+// Note: keep the original order of the names in the output.
+
+const friend = (friends) => friends.filter((friend) => friend.length === 4);
